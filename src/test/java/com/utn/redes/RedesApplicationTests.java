d661e7176c974f2af17bb4829f9881c9bc530315
Bautista Marquez <bmarquez@frba.utn.edu.ar>
2024-05-27 15:45:50 -0300

    Initial Commit

---
+++ src/test/java/com/utn/redes/RedesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.redes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedesApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
