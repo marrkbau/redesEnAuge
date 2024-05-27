e89f05ed02342d31fa8fcdcb26b58278b0728dcd
DanteBall <dntball@gmail.com>
2024-05-27 16:21:12 -0300

    clase Persistente

---
+++ src/main/java/com/utn/redes/Persistente.java
@@ -0,0 +1,17 @@
+package com.utn.redes;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@Entity
+@MappedSuperclass
+public class Persistente {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+
+}
--- src/main/java/com/utn/redes/perfil/Perfil.java
+++ src/main/java/com/utn/redes/perfil/Perfil.java
@@ -1,12 +1,24 @@
 package com.utn.redes.perfil;
 
+import com.utn.redes.Persistente;
 import com.utn.redes.publicacion.Publicacion;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.util.List;
 
-public class Perfil {
+@Entity
+@Table(name = "perfil")
+public class Perfil extends Persistente {
+  @Column(name = "sitio_web")
   private String sitioWeb;
+
+  @Column(name = "ubicacion")
   private String ubicacion;
+
+  @Column()
   private String urlFotoPerfil;
   private Usuario usuario;
   private List<Publicacion> publicaciones;
